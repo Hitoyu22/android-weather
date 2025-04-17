28d64fc7b67b4196444ffb61c70d2287a85400d2
Rémy <remy.thibaut2005@gmail.com>
2025-04-17 23:10:30 +0200

    Ajout mode sombre + page de settings fonctionnels (avec save en SharedPreferences)

--- app/src/main/java/fr/esgi/android/weather/activities/HomeActivity.kt
+++ app/src/main/java/fr/esgi/android/weather/activities/HomeActivity.kt
@@ -1,6 +1,7 @@
 package fr.esgi.android.weather.activities
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.os.Handler
@@ -37,23 +38,33 @@ class HomeActivity : WeatherActivity(R.layout.activity_main, R.id.home) {
 
         val imageView = findViewById<ImageView>(R.id.imageView)
         imageView.setImageDrawable(getWeatherDrawable())
-    }
 
-    private fun getWeatherDrawable(): Drawable? {
-        val weatherType = "cloudy"
-        val isDarkMode = AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES
+        val sharedPreferences = getSharedPreferences("app_preferences", MODE_PRIVATE)
+        val isNightModeEnabled = sharedPreferences.getBoolean("night_mode_enabled", false)
 
-        return when (weatherType.lowercase()) {
-            "sunny" -> loadImageFromDrawable(isDarkMode, "sunny")
-            "cloudy" -> loadImageFromDrawable(isDarkMode, "cloudy")
-            "rainy" -> loadImageFromDrawable(isDarkMode, "rainy")
-            "snow" -> loadImageFromDrawable(isDarkMode, "snow")
-            "thunder" -> loadImageFromDrawable(isDarkMode, "thunder")
-            else -> loadImageFromDrawable(isDarkMode, "sunny")
+        if (isNightModeEnabled) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         }
     }
 
-    private fun loadImageFromDrawable(isDarkMode: Boolean, imageName: String): Drawable? {
+    private fun getWeatherDrawable(): Drawable? {
+        val weatherType = "sunny"
+        val isDarkMode = AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES
+
+        return when (weatherType.lowercase()) {
+            "sunny" -> loadImage(isDarkMode, "sunny")
+            "cloudy" -> loadImage(isDarkMode, "cloudy")
+            "rainy" -> loadImage(isDarkMode, "rainy")
+            "snow" -> loadImage(isDarkMode, "snow")
+            "thunder" -> loadImage(isDarkMode, "thunder")
+            else -> loadImage(isDarkMode, "sunny")
+        }
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables", "DiscouragedApi")
+    private fun loadImage(isDarkMode: Boolean, imageName: String): Drawable? {
         val imageNameWithSuffix = if (isDarkMode) {
             imageName + "_night"
         } else {
@@ -70,9 +81,4 @@ class HomeActivity : WeatherActivity(R.layout.activity_main, R.id.home) {
         return resources.getDrawable(resourceId, null)
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-
-        // handler.removeCallbacks(notificationRunnable)
-    }
 }
