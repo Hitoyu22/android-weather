d1f5f531c4a4f3f771308192cd41e0d2f109459e
Rémy <remy.thibaut2005@gmail.com>
2025-04-17 22:13:26 +0200

    Ajout des illustrations sur la page d'accueil (en dur)

--- app/src/main/java/fr/esgi/android/weather/activities/HomeActivity.kt
+++ app/src/main/java/fr/esgi/android/weather/activities/HomeActivity.kt
@@ -1,11 +1,14 @@
 package fr.esgi.android.weather.activities
 
 import android.Manifest
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import android.widget.ImageView
 import androidx.annotation.RequiresPermission
+import androidx.appcompat.app.AppCompatDelegate
 import fr.esgi.android.weather.R
 import fr.esgi.android.weather.network.WeatherAPI
 import fr.esgi.android.weather.notifications.NotificationHelper
@@ -28,13 +31,48 @@ class HomeActivity : WeatherActivity(R.layout.activity_main, R.id.home) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        notificationHelper = NotificationHelper(this)
+        // notificationHelper = NotificationHelper(this)
 
-        handler.post(notificationRunnable)
+        // handler.post(notificationRunnable)
+
+        val imageView = findViewById<ImageView>(R.id.imageView)
+        imageView.setImageDrawable(getWeatherDrawable())
+    }
+
+    private fun getWeatherDrawable(): Drawable? {
+        val weatherType = "cloudy"
+        val isDarkMode = AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES
+
+        return when (weatherType.lowercase()) {
+            "sunny" -> loadImageFromDrawable(isDarkMode, "sunny")
+            "cloudy" -> loadImageFromDrawable(isDarkMode, "cloudy")
+            "rainy" -> loadImageFromDrawable(isDarkMode, "rainy")
+            "snow" -> loadImageFromDrawable(isDarkMode, "snow")
+            "thunder" -> loadImageFromDrawable(isDarkMode, "thunder")
+            else -> loadImageFromDrawable(isDarkMode, "sunny")
+        }
+    }
+
+    private fun loadImageFromDrawable(isDarkMode: Boolean, imageName: String): Drawable? {
+        val imageNameWithSuffix = if (isDarkMode) {
+            imageName + "_night"
+        } else {
+            imageName
+        }
+
+        val resourceId = resources.getIdentifier(imageNameWithSuffix, "drawable", packageName)
+
+        if (resourceId == 0) {
+            Log.e("HomeActivity", "Resource not found: $imageNameWithSuffix")
+            return null
+        }
+
+        return resources.getDrawable(resourceId, null)
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        handler.removeCallbacks(notificationRunnable)
+
+        // handler.removeCallbacks(notificationRunnable)
     }
 }
