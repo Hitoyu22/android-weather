0ec5a65ccc8cac064b448722c05fbacb0e09c3cd
Rémy <remy.thibaut2005@gmail.com>
2025-04-28 21:19:29 +0200

    Update Settings Page
    - Better Theme picker
    - Language could be updated in the app

--- app/src/main/java/fr/esgi/android/weather/activities/SettingsActivity.kt
+++ app/src/main/java/fr/esgi/android/weather/activities/SettingsActivity.kt
@@ -1,34 +1,79 @@
 package fr.esgi.android.weather.activities
 
+import android.content.Context
+import android.content.res.Configuration
 import android.os.Bundle
-import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.appcompat.widget.SwitchCompat
 import fr.esgi.android.weather.R
 import fr.esgi.android.weather.WeatherApp
+import java.util.Locale
 
 class SettingsActivity : WeatherActivity(R.layout.activity_settings, R.id.settings) {
 
-    private lateinit var darkMode: SwitchCompat
+    private lateinit var themeSetting: TextView
+    private lateinit var languageSetting: TextView
+
+    private val languageFrench = "Français"
+    private val languageEnglish = "English"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        findViewById<Button>(R.id.apply_settings).setOnClickListener {
-            savePreferences()
+        themeSetting = findViewById(R.id.theme_setting)
+        languageSetting = findViewById(R.id.language_setting)
+
+        themeSetting.setOnClickListener {
+            showThemeDialog()
         }
 
-        darkMode = findViewById(R.id.dark_mode_switch)
-        darkMode.isChecked = (application as WeatherApp).isDarkMode()
+        languageSetting.setOnClickListener {
+            showLanguageDialog()
+        }
     }
 
-    private fun savePreferences() {
-        (application as WeatherApp).setDarkMode(darkMode.isChecked)
-
-        AppCompatDelegate.setDefaultNightMode(
-            if (darkMode.isChecked) AppCompatDelegate.MODE_NIGHT_YES
-            else AppCompatDelegate.MODE_NIGHT_NO
+    private fun showThemeDialog() {
+        val items = arrayOf(
+            getString(R.string.light_mode),
+            getString(R.string.dark_mode),
+            getString(R.string.system_default)
         )
+
+        AlertDialog.Builder(this)
+            .setTitle(R.string.select_theme)
+            .setItems(items) { _, which ->
+                when (which) {
+                    0 -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    1 -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    2 -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                }
+            }
+            .show()
     }
 
+    private fun showLanguageDialog() {
+        val languages = arrayOf(languageFrench, languageEnglish)
+
+        AlertDialog.Builder(this)
+            .setTitle(R.string.select_language)
+            .setItems(languages) { _, which ->
+                when (which) {
+                    0 -> setLanguage("fr")
+                    1 -> setLanguage("en")
+                }
+            }
+            .show()
+    }
+
+    private fun setLanguage(languageCode: String) {
+        val locale = Locale(languageCode)
+        Locale.setDefault(locale)
+
+        val config = Configuration()
+        config.setLocale(locale)
+
+        baseContext.resources.updateConfiguration(config, baseContext.resources.displayMetrics)
+        recreate()
+    }
 }
